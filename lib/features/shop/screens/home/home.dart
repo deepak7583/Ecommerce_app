de9b28d406a582c2b735bc92fdcc1fdda5d14d7d
deepak11300 <deepak.cerebulb@gmail.com>
2024-05-23 18:03:57 +0530

    [=]minor changes

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -24,6 +24,7 @@ class HomeScreen extends StatelessWidget {
               child: Column(
                 children: [
                   HomeAppBar(),
+
                   /// searchbar
                   SizedBox(
                     height: TSizes.spaceBtwItems,
